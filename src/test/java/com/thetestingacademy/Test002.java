e902c3b9881a026af1ea83afecd4793ac42620e2
sravanthi <sravanthi.golla@gmail.com>
2024-09-18 21:45:06 +0530

    sout1

--- src/test/java/com/thetestingacademy/Test002.java
+++ src/test/java/com/thetestingacademy/Test002.java
@@ -1,7 +1,10 @@
 package com.thetestingacademy;
 
+import sun.jvm.hotspot.HelloWorld;
+
 public class Test002 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("HelloWorld");
     }
 }
